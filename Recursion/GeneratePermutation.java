93ca6d58e9c1fd8a316acb4021836d6133c3f48f
Ashvani Sharma <panditashuabvp@gmail.com>
2025-07-29 03:32:10 +0530

    Generate Permutation

---
+++ Recursion/GeneratePermutation.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class GeneratePermutation {
+    public static void main(String[] args) {
+        String st="abca";
+        printPermutation(st,"");
+    }
+    public static void printPermutation(String st, String ans){
+        if(st.length()==0){
+            System.out.println(ans+" ");
+            return;
+        }
+        for(int i=0;i<st.length();i++){
+            char ch=st.charAt(i);
+            String s1=st.substring(0,i);
+            String s2=st.substring(i+1);
+            printPermutation(s1+s2,ans+ch);
+        }
+    }
+}
