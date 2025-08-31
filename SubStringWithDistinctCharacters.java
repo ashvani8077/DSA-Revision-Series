1aa5e2881eaa2847e79f3773aa5852f0b32393a8
Ashvani Sharma <panditashuabvp@gmail.com>
2025-08-31 14:04:03 +0530

    SubStringWithDistinctCharacters

---
+++ SubStringWithDistinctCharacters.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubStringWithDistinctCharacters {
+    public static void main(String[] args) {
+        String s="xyzzaz";
+        count(s);
+    }
+    public static void count(String s){
+        int i=0;
+        int j=0;
+        int k=2;
+        double d=6.2;
+        List<Double>di=new ArrayList<>();
+        System.out.printf("%.6f",d);
+        System.out.println();
+        di.add(d);
+        System.out.println(di);
+    }
+}
